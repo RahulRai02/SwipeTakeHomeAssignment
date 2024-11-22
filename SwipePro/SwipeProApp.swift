6008912eba072d5196172ff640945ef15233b055
Rahul Rai <rahul@Rahuls-MacBook-Air-2.local>
2024-11-22 16:27:25 +0530

    Initial Commit

---
+++ SwipePro/SwipeProApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeProApp.swift
+//  SwipePro
+//
+//  Created by Rahul Rai on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
